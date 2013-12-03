c725e68d65b3ce8968b707d1764bcba1aa609088
System Administrator <root@Nitins-MacBook-Pro.local>
2013-12-02 22:19:13 -0800

    adding the updated version

--- sp_ranking_regex_creator.pl
+++ sp_ranking_regex_creator.pl
@@ -1,25 +1,35 @@
 #Utility program to create the regex for search and promote
+#Author : Nitin Bertie Eusebius
+#Status : Open Source
 
+#main menu call to kick start the programm
 main_menu();
 
+#controls the main execution of the programm
 sub main_menu{
 	
+	#asks the users for a choice
 	print "How do you want to create the regex?\n";
 	print "For legacy way press L or new way press N\n";
 	$choice = <>;
-	print "You selected -->$choice";
-
+	
+	#removes the new line
 	chomp($choice);
-
+	#lower case the choice for ease
 	$choice = lc($choice);
 
 	if($choice eq "l"){
+		
 		createLegacyRegex();
 		start_over_option();
+	
 	}elsif($choice eq "n"){
+		
 		createNewRegex();
 		start_over_option();
+	
 	}else{
+		
 		print "Wrong choice..I am aborting myself\n";
 		exit();
 	}
@@ -33,27 +43,34 @@ sub start_over_option{
 	$choice = lc($choice);
 	
 	if($choice eq "y"){
+		
 		main_menu();
+	
 	}elsif($choice eq "n"){
-		print "Thanks for using this utility program...Good Bye...";
+		
+		print "Thanks for using this utility program...Good Bye...\n";
+	
 	}else{
+		
 		print "Wrong choice..I am aborting myself\n";
 		exit();
 	}
 }
 
 
+#legacy regex will not consider the decimal values
 sub createLegacyRegex{
 	
 	print "How many max digit you want to create?";
-	$max_digit = <>;
-	$regex_for_digit = "";
+	my $max_digit = <>;
+	my $regex_for_digit = "";
 
 	my $temp;
 	my @ranking_regex_array;
 
 	for ($i=1;$i<=$max_digit;$i++){
 
+		#special consideration for max digit of 1
 		if ($i==1) {
 			$temp = "regexp "."[[:digit:]]";
         	print "Enter ranking value for ".$temp." ";
@@ -64,22 +81,28 @@ sub createLegacyRegex{
             push(@ranking_regex_array,$temp." ".$ranking_value);
 		}
 		else{
-		
+			#create the [[:digit:]] for the total digits to be created
 			for($j=1;$j<$i;$j++){
 				$regex_for_digit = $regex_for_digit."[[:digit:]]";
 		}
 	
+		#now run the programm for 0-9 against the regex string created above
 		for($k=1;$k<=9;$k++){
 			$temp = "regexp "."$k".$regex_for_digit;
 			print "Enter ranking value for ".$temp." ";
         	my $ranking_value = <>;
 			
 				if($ranking_value == ""){
+					
 					$ranking_value = "0\n";
+				
 				}
-        		push(@ranking_regex_array,$temp." ".$ranking_value);
+        		
+				push(@ranking_regex_array,$temp." ".$ranking_value);
 			}
 	}
+	
+	#reset this value so that correct number of digits can be calculated
 	$regex_for_digit = "";
 }
 
@@ -87,6 +110,7 @@ print "***Printing the final values of regexes***\n";
 print "***You can copy paste this to s&p***\n";
 	
 	for($i=$#ranking_regex_array;$i>=0;$i--){
+		
 		print $ranking_regex_array[$i];
 	
 	}
@@ -96,8 +120,8 @@ print "***You can copy paste this to s&p***\n";
 sub createNewRegex{
 	
 	print "How many max digit you want to create?";
-	$max_digit = <>;
-	$regex_for_digit = "";
+	my $max_digit = <>;
+	my $regex_for_digit = "";
 
 	my $temp;
 	my @ranking_regex_array;
@@ -109,9 +133,11 @@ sub createNewRegex{
         	print "Enter ranking value for ".$temp." ";
 			my $ranking_value = <>;
 			if($ranking_value == ""){
+					
 				$ranking_value = "0\n";
 			}
-            push(@ranking_regex_array,$temp." ".$ranking_value);
+            
+			push(@ranking_regex_array,$temp." ".$ranking_value);
 		}
 		else{
 		
@@ -132,6 +158,7 @@ sub createNewRegex{
 			push(@ranking_regex_array,$temp." ".$ranking_value);
 		}
 	}
+	$regex_for_digit = "";
 }
 
 print "***Printing the final values of regexes***\n";
